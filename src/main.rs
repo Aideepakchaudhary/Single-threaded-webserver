0fc64e1bbe83a7aeda3d08bde04280890526ef84
Aideepakchaudhary <aideepakchaudhary@gmail.com>
2023-11-13 20:35:01 +0530

    setup the node

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap(); // It listen to the tcp socket server and bind it with the address.
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Connection established!");
+    }
 }
